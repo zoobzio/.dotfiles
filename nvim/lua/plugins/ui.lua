2bf6bf7d6f3ecda9d035514b9b0cdc23f85b0522
Alex Thorwaldson <alex@zoobz.io>
2024-05-08 12:04:46 -0700

    nvim: disable notifications

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -24,30 +24,30 @@ return {
 	},
 
 	-- notifications
-	{
-		"rcarriga/nvim-notify",
-		opts = {
-			render = "wrapped-compact",
-			icons = {
-				DEBUG = icons.diagnostics.Debug,
-				ERROR = icons.diagnostics.Error,
-				INFO = icons.diagnostics.Info,
-				TRACE = icons.diagnostics.Trace,
-				WARN = icons.diagnostics.Warn,
-			},
-			timeout = 3000,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-			on_open = function(win)
-				vim.api.nvim_win_set_config(win, { zindex = 100 })
-			end,
-		},
-		init = function()
-			vim.notify = require("notify")
-		end,
-	},
+	--[[{]]
+		--[["rcarriga/nvim-notify",]]
+		--[[opts = {]]
+			--[[render = "wrapped-compact",]]
+			--[[icons = {]]
+				--[[DEBUG = icons.diagnostics.Debug,]]
+				--[[ERROR = icons.diagnostics.Error,]]
+				--[[INFO = icons.diagnostics.Info,]]
+				--[[TRACE = icons.diagnostics.Trace,]]
+				--[[WARN = icons.diagnostics.Warn,]]
+			--[[},]]
+			--[[timeout = 3000,]]
+			--[[max_height = function()]]
+				--[[return math.floor(vim.o.lines * 0.75)]]
+			--[[end,]]
+			--[[max_width = function()]]
+				--[[return math.floor(vim.o.columns * 0.75)]]
+			--[[end,]]
+			--[[on_open = function(win)]]
+				--[[vim.api.nvim_win_set_config(win, { zindex = 100 })]]
+			--[[end,]]
+		--[[},]]
+		--[[init = function()]]
+			--[[vim.notify = require("notify")]]
+		--[[end,]]
+	--[[},]]
 }
